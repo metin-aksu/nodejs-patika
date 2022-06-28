8e1b6ba701d7d453a56c269d3bb7f9071a93fcb2
Metin AKSU <drmetin28@hotmail.com>
2022-06-28 03:30:07 +0300

    daireAlan.js güncellemesi

--- daireAlan.js
+++ daireAlan.js
@@ -1,9 +1,9 @@
 
-const param = process.argv.slice(2);
+const yaricap = process.argv[2];
 const pi = Math.PI;
 
-function daireAlan(yaricap){
-    console.log("Yarıçapı (", yaricap,") olan dairenin alanı : ", pi*yaricap**2);
+function daireAlan(r){
+    console.log("Yarıçapı (", r,") olan dairenin alanı : ", pi*r**2);
 }
 
-daireAlan(param[0]*1);
+daireAlan(yaricap*1);
