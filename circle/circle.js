6f23acd26478091a59753ec05c1e4405b9287b8d
Metin AKSU <drmetin28@hotmail.com>
2022-07-02 12:27:10 +0300

    circle module added

---
+++ circle/circle.js
@@ -0,0 +1,9 @@
+function circleArea(radius) {
+    return Math.PI * radius * radius;
+}
+
+function circleCircumference(radius) {
+    return 2 * Math.PI * radius;
+}
+
+export { circleArea, circleCircumference };
---
+++ circle/index.js
@@ -0,0 +1,4 @@
+import { circleArea, circleCircumference } from "./circle.js";
+
+console.log("Area of circle: " + circleArea(5));
+console.log("Circumference of circle: " + circleCircumference(5));
